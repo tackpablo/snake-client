073f34e3f508071a8ad7a1f1e388c4d9b17c321d
Pablo Tack <tackpablo@gmail.com>
2022-02-16 08:12:23 -0800

    move commands tests

--- client.js
+++ client.js
@@ -21,7 +21,36 @@ const connect = function () {
     conn.write("Name: PTA");
   });
 
+  // // sending move up command
+  // conn.on("connect", () => {
+  //   conn.write("Move: up");
+
+  //   // setInterval to move up snake every 50ms
+  //   setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 50);
+
+  //   // successive msgs once moving up
+  //   console.log("Moving up");
+  //   console.log("Moving up in the world");
+
+  //   // delayed msgs once moving up
+  //   setTimeout(() => {
+  //     console.log("Moving up");
+  //   }, 50);
+
+  //   setTimeout(() => {
+  //     console.log("Moving up in the world");
+  //   }, 100);
+  // });
+
   return conn;
 };
 
 module.exports = { connect };
+
+// Notes
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
